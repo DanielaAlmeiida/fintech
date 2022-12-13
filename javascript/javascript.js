51505fff1487118ad34089b0bce9c25ab8e2b19e
Daniela Almeida <danielaspbrasil@gmail.com>
2022-12-12 22:28:35 -0300

    Adicionado todos os arquivos.

---
+++ javascript/javascript.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    let enviar = document.querySelector('form button.btn');
+    enviar.addEventListener('click', () => {
+        location.href = "paginas/dashboard.html";
+    })
+
+}
